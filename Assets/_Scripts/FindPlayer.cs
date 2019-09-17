762e07dcf200b8bd01add1b12891a5d8cc7f6859
slevy2020 <slevy@chadwickschool.org>
2019-09-17 11:43:50 -0700

    feat: add basic player movement and follow w/ camera

---
+++ Assets/_Scripts/FindPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using Cinemachine;
+
+public class FindPlayer : NetworkBehaviour {
+  public CinemachineFreeLook camera;
+  private bool playerFound = false;
+
+  void Start() {
+    camera = GetComponent<CinemachineFreeLook>();
+  }
+
+  void Update() {
+    if (!isLocalPlayer) {
+      return;
+    }
+  }
+
+  public void Find() {
+    if (!playerFound) {
+      camera.Follow = GameObject.FindGameObjectWithTag("Player").transform;
+      camera.LookAt = GameObject.FindGameObjectWithTag("Player").transform;
+      playerFound = true;
+    }
+  }
+}
