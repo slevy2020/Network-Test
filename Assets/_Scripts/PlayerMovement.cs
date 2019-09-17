790f604bd38f2e4590ff65a159e9f80560d08e2d
slevy2020 <slevy@chadwickschool.org>
2019-09-17 10:44:25 -0700

    feat: add camera and basic network
    need to program movement and have the cinemachine camera find the player once they spawn

---
+++ Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerMovement : NetworkBehaviour {
+
+  void Update() {
+    if (!isLocalPlayer) {
+      return;
+    }
+
+    //code to move
+  }
+}
