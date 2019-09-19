54376ddc128f7ed3fd45d1eff89086618f3d16fc
slevy2020 <slevy@chadwickschool.org>
2019-09-19 11:31:46 -0700

    feat: add proper player turning

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -7,10 +7,19 @@ public class PlayerMovement : NetworkBehaviour {
   public float speed = 6f;
   public float gravity = -9.8f;
 
+  private float turnSmoothVelocity;
+  public float turnSmoothTime = 3.0f;
+
   public CharacterController cc;
 
   void Start() {
+    // if (!isLocalPlayer) {
+    //   return;
+    // }
     GameObject.FindGameObjectWithTag("Camera").GetComponent<FindPlayer>().SendMessage("Find");
+
+    Color random = new Color (Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+    GetComponent<Renderer>().material.color = random;
   }
 
   void Update() {
@@ -26,6 +35,11 @@ public class PlayerMovement : NetworkBehaviour {
     movement.y = gravity;
     movement *= Time.deltaTime;
     movement = transform.TransformDirection(movement);
+
+    Vector2 inputDir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    inputDir = inputDir.normalized;
+    float targetRot = Mathf.Atan2(inputDir.x, inputDir.y)*Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
+    transform.eulerAngles = Vector3.up*Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRot, ref turnSmoothVelocity, turnSmoothTime);
 
     cc.Move(movement);
   }
