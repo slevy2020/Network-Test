faa5111c0affe96fe25b988ddfc60759a58c2b63
slevy2020 <slevy@chadwickschool.org>
2019-09-19 12:44:47 -0700

    feat: add somewhat working jumping
    speed was scaled way down for some reason, still trying to figure out why

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -6,9 +6,17 @@ using Mirror;
 public class PlayerMovement : NetworkBehaviour {
   public float speed = 6f;
   public float gravity = -9.8f;
+	public float jumpSpeed = 15.0f;
+  private float vertSpeed;
+	public float terminalVelocity = -20.0f;
+	public float minFall = -1.5f;
+	public float pushForce = 3.0f;
+	private ControllerColliderHit contact;
 
   private float turnSmoothVelocity;
   public float turnSmoothTime = 3.0f;
+
+  [SyncVar(hook = nameof(ChangeColor))] public Color random;
 
   public CharacterController cc;
 
@@ -16,10 +24,15 @@ public class PlayerMovement : NetworkBehaviour {
     if (!isLocalPlayer) {
       return;
     }
-    GameObject.FindGameObjectWithTag("Camera").GetComponent<FindPlayer>().SendMessage("Find", this.gameObject);
 
-    Color random = new Color (Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
-    GetComponent<Renderer>().material.color = random;
+    vertSpeed = minFall;
+
+    GameObject.FindGameObjectWithTag("Camera").GetComponent<FindPlayer>().SendMessage("Find", this.gameObject);
+    random = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+  }
+
+  void ChangeColor(Color col) {
+      GetComponent<Renderer>().material.color = col;
   }
 
   void Update() {
@@ -41,6 +54,49 @@ public class PlayerMovement : NetworkBehaviour {
     float targetRot = Mathf.Atan2(inputDir.x, inputDir.y)*Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
     transform.eulerAngles = Vector3.up*Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRot, ref turnSmoothVelocity, turnSmoothTime);
 
+    bool hitGround = false;
+    RaycastHit hit;
+    if (vertSpeed < 0 && Physics.Raycast(transform.position, Vector3.down, out hit)) {
+      float check = (cc.height + cc.radius) / 1.9f;
+      hitGround = hit.distance <= check;	// to be sure check slightly beyond bottom of capsule
+    }
+
+    if (hitGround) {
+			if (Input.GetButtonDown("Jump")) {
+				vertSpeed = jumpSpeed;
+			} else {
+				vertSpeed = minFall;
+				// _animator.SetBool("Jumping", false);
+			}
+		} else {
+			vertSpeed += gravity * 5 * Time.deltaTime;
+			if (vertSpeed < terminalVelocity) {
+				vertSpeed = terminalVelocity;
+			}
+			   // if (contact != null ) {	// not right at level start
+		     // 	_animator.SetBool("Jumping", true);
+		     // }
+		// 	if (cc.isGrounded) {
+		// 		if (Vector3.Dot(movement, contact.normal) < 0) {
+		// 			movement = contact.normal * speed;
+		// 		} else {
+		// 			movement += contact.normal * speed;
+		// 		}
+		// }
+		}
+		movement.y = vertSpeed;
+    movement *= Time.deltaTime;
+
     cc.Move(movement);
   }
+
+  // store collision to use in Update
+	void OnControllerColliderHit(ControllerColliderHit hit) {
+		contact = hit;
+
+		Rigidbody body = hit.collider.attachedRigidbody;
+		if (body != null && !body.isKinematic) {
+			body.velocity = hit.moveDirection * pushForce;
+		}
+	}
 }
